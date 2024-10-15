3749a625ffaaf35a96e77c61aa2b41b990b9e968
kishmwangi <kishmwangi31@gmail.com>
2024-10-15 18:22:03 +0300

    app update

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,21 +19,52 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
+@app.route('/bakeries', methods=['GET'])
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return jsonify([{
+        'id': bakery.id,
+        'name': bakery.name,
+        'location': bakery.location
+    } for bakery in bakeries])
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET'])
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery is None:
+        return make_response(jsonify({'error': 'Bakery not found'}), 404)
 
-@app.route('/baked_goods/by_price')
+    return jsonify({
+        'id': bakery.id,
+        'name': bakery.name,
+        'location': bakery.location,
+        'baked_goods': [{
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price
+        } for baked_good in bakery.baked_goods]
+    })
+
+@app.route('/baked_goods/by_price', methods=['GET'])
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([{
+        'id': baked_good.id,
+        'name': baked_good.name,
+        'price': baked_good.price
+    } for baked_good in baked_goods])
 
-@app.route('/baked_goods/most_expensive')
+@app.route('/baked_goods/most_expensive', methods=['GET'])
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good is None:
+        return make_response(jsonify({'error': 'No baked goods found'}), 404)
+
+    return jsonify({
+        'id': baked_good.id,
+        'name': baked_good.name,
+        'price': baked_good.price
+    })
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
